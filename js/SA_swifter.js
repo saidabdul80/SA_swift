cc28abb3399e9948c60369352c9cb8af2412a8c9
Said Abdulsalam <42839808+saidabdul80@users.noreply.github.com>
2020-12-18 18:54:17 +0000

    first commit

---
+++ js/SA_swifter.js
@@ -0,0 +1,67 @@
+class SA_SwiftMenu {
+	  constructor(wrapper, radius, menu, tabs) {	  	
+	    this.wrapper = wrapper;
+	    this.radius = radius;
+	    this.menu = menu;
+	    this.tabs = tabs;
+	    this.stateDataShow = false;
+	  }
+	  switf() {	  		  	
+	    var $vm = this;
+		  this.wrapper.on('click', function(){
+		  	var radius = $vm.radius;
+			var fields = $vm.tabs,
+			container = $vm.menu, 
+			width = container.width(),
+			height = container.height();
+
+
+		if ($vm.stateDataShow === false) {
+
+			fields.css('display','block');
+			$vm.stateDataShow = true;
+
+			if (fields.length <4) {				
+				//determining interval of menus if less dan 4 they should come closer
+				var angle = 0, step = (2*Math.PI) / 6;			
+			}else{
+				//determining interval of menus
+				var angle = 0, step = (2*Math.PI) / fields.length;
+			}
+
+			fields.each(function() {
+			    var x = Math.round(width/2 + width * Math.cos(angle) - $(this).width()/2);
+			    var y = Math.round(height/2 + width * Math.sin(angle) - $(this).height()/2);
+			    /*if(window.console) {
+			        //console.log($(this).text(), x, y);
+			    }*/
+			    $(this).css({
+			        left: x + 'px',
+			        top: y + 'px'
+			    });
+			    angle += step;
+			})
+		}else if ($vm.stateDataShow === true) {
+			//return menu back to ==> hide splitted menu
+			fields.each(function() {			    
+			    $(this).css({
+			        left: '0px',
+			        top: '0px'
+			    });
+			    angle += step;
+			})
+			setTimeout(function() {
+				$vm.stateDataShow = false;
+				fields.css('display','none');
+
+			}, 100);
+
+		}
+		});
+
+		  $(window).click(function() {
+			//Hide the menus if visible
+		  });
+	  }
+
+	}
